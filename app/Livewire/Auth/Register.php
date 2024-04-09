578c93bc94bb7a841ac125e1a1dd1ed96ad17760
matheus8695 <0821matheus@gmail.com>
2024-04-09 09:45:00 -0300

    CRM-1: be able to register in the application

---
+++ app/Livewire/Auth/Register.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+
+class Register extends Component
+{
+    #[Rule(['required', 'max:255'])]
+    public ?string $name = null;
+
+    #[Rule(['required'])]
+    public ?string $password = null;
+
+    #[Rule(['required', 'email', 'max:255', 'confirmed'])]
+    public ?string $email = null;
+
+    public ?string $email_confirmation = null;
+
+    public function render(): View
+    {
+        return view('livewire.auth.register');
+    }
+
+    public function submit(): void
+    {
+        $this->validate();
+
+        $user = User::query()->create([
+            'name'     => $this->name,
+            'email'    => $this->email,
+            'password' => $this->password,
+        ]);
+
+        auth()->login($user);
+
+        $this->redirect(RouteServiceProvider::HOME);
+    }
+}
---
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Livewire\Auth\Register;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Livewire\Livewire;
+
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
+
+it('renders successfully', function () {
+    Livewire::test(Register::class)
+        ->assertOk();
+});
+
+it('should be able to register a new user in the system', function () {
+    Livewire::test(Register::class)
+        ->set('name', 'joe doe')
+        ->set('email', 'joe@doe.com')
+        ->set('email_confirmation', 'joe@doe.com')
+        ->set('password', 'password')
+        ->call('submit')
+        ->assertHasNoErrors()
+        ->assertRedirect(RouteServiceProvider::HOME);
+
+    assertDatabaseHas('users', [
+        'name'  => 'Joe doe',
+        'email' => 'joe@doe.com',
+    ]);
+
+    assertDatabaseCount('users', 1);
+
+    expect(auth()->check())
+        ->and(auth()->user())
+        ->id->toBe(User::first()->id);
+});
+
+it('validation rules', function ($f) {
+    Livewire::test(Register::class)
+        ->set($f->field, $f->value)
+        ->call('submit')
+        ->assertHasErrors([$f->field => $f->rule]);
+})->with([
+    'name::required'     => (object)['field' => 'name', 'value' => '', 'rule' => 'required'],
+    'name::max:255'      => (object)['field' => 'name', 'value' => str_repeat('*', 256), 'rule' => 'max'],
+    'email::required'    => (object)['field' => 'email', 'value' => '', 'rule' => 'required'],
+    'email::email'       => (object)['field' => 'email', 'value' => 'not-an-email', 'rule' => 'email'],
+    'email::max:255'     => (object)['field' => 'email', 'value' => str_repeat('*' . '@doe.com', 256), 'rule' => 'max:255'],
+    'email::confirmed'   => (object)['field' => 'email', 'value' => 'joe@doe.com', 'rule' => 'confirmed'],
+    'password::required' => (object)['field' => 'password', 'value' => '', 'rule' => 'required'],
+]);
