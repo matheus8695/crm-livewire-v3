0e1a4db99ab5f125a49741e60d8b97d6814cadf0
matheus8695 <0821matheus@gmail.com>
2024-04-09 10:14:16 -0300

    CRM-1: register form front-end

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
-</div>
+<x-card shadow class="mx-auto w-[350px]">
+    <x-form wire:submit="submit">
+        <x-input label="Name" wire:model="name" />
+        <x-input label="Email" wire:model="email" />
+        <x-input label="Confirm your email" wire:model="email_confirmation" />
+        <x-input label="Password" wire:model="password" type="password"/>
+
+        <x-slot:actions>
+            <x-button label="Cancel" />
+            <x-button label="Click me!" class="btn-primary" type="submit" spinner="save" />
+        </x-slot:actions>
+    </x-form>
+</x-card>
