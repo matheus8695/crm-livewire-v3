64840c6eb40aae4b5db146db00c72cd9bfe12e99
matheus8695 <0821matheus@gmail.com>
2024-04-19 10:32:55 -0300

    CRM-2: frontend of logout button

--- resources/views/livewire/auth/logout.blade.php
+++ resources/views/livewire/auth/logout.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate wire:click="logout"/>
 </div>
