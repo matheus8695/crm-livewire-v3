7bbb6e4a808c4860c68d729a11db449cf292e0af
matheus8695 <0821matheus@gmail.com>
2024-05-03 10:17:55 -0300

    CRM-5: creating a seed for permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,12 +8,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            PermissionSeeder::class,
+        ]);
 
         User::factory()->create([
             'name'  => 'Test User',
