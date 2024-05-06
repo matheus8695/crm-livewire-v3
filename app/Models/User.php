249879ca11db1482547138c80bbe7eadc7c9a501
matheus8695 <0821matheus@gmail.com>
2024-05-06 16:32:54 -0300

    CRM-5: using cache to store/retrieve user permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cache;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -39,10 +42,26 @@ class User extends Authenticatable
     public function givePermissionTo(string $key): void
     {
         $this->permissions()->firstOrCreate(compact('key'));
+
+        Cache::forget($this->getPermissionCacheKey());
+        Cache::rememberForever(
+            $this->getPermissionCacheKey(),
+            fn () => $this->permissions
+        );
     }
 
     public function hasPermissionTo(string $key): bool
     {
-        return $this->permissions()->where(compact('key'))->exists();
+        /** @var Collection $permissions */
+        $permissions = Cache::get($this->getPermissionCacheKey(), $this->permissions);
+
+        return $permissions
+            ->where('key', '=', $key)
+            ->isNotEmpty();
+    }
+
+    private function getPermissionCacheKey(): string
+    {
+        return "user::{$this->id}::permissions";
     }
 }
