7d38ea2666acf323013f77c1eff4713cab2600a2
matheus8695 <0821matheus@gmail.com>
2024-05-15 10:20:17 -0300

    CRM-26: making sure that the route is protected by the permission BE_AN_ADMIN

--- tests/Feature/Admin/UserManagement/ListUsersTest.php
+++ tests/Feature/Admin/UserManagement/ListUsersTest.php
@@ -9,3 +9,9 @@ it('should be able to access the route admin/users', function () {
 
     get(route('admin.users'))->assertOk();
 });
+
+test('making sure that the route is protected by the permission BE_AN_ADMIN', function () {
+    actingAs(User::factory()->create());
+
+    get(route('admin.users'))->assertForbidden();
+});
