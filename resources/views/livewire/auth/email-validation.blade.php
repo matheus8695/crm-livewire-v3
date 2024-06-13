c23861c7e83d241087b78f89e4fff060a2105388
matheus8695 <0821matheus@gmail.com>
2024-06-13 11:00:06 -0300

    CRM-11: frontend: creating our email-validation page

--- resources/views/livewire/auth/email-validation.blade.php
+++ resources/views/livewire/auth/email-validation.blade.php
@@ -1,3 +1,25 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-</div>
+<x-card title="Email Validation" shadow class="mx-auto w-[500px]">
+
+    @if($sendNewCodeMessage)
+        <x-alert icon="o-envelope" class="alert-warning mb-4">
+            {{ $sendNewCodeMessage }}
+        </x-alert>
+    @endif
+
+    <x-form wire:submit="handle">
+        <p>We sent a cade. Please check your email</p>
+        <x-input label="Code" wire:model="code" />
+
+        <x-slot:actions>
+            <div class="w-full flex items-center justify-between">
+                <a wire:click='sendNewCode' class="link link-primary">
+                    Send a new code
+                </a>
+
+                <div>
+                    <x-button label="Check Code" class="btn-primary" type="submit" spinner="submit" />
+                </div>
+            </div>
+        </x-slot:actions>
+    </x-form>
+</x-card>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
 use App\Enum\Can;
-use App\Livewire\Auth\{Login, Password, Register};
+use App\Livewire\Auth\{EmailValidation, Login, Password, Register};
 use App\Livewire\{Admin, Welcome};
 use Illuminate\Support\Facades\{Auth, Route};
 
 #auth
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('auth.register');
-Route::get('/email-validation', fn () => 'oi')->middleware('auth')->name('auth.email-validation');
+Route::get('/email-validation', EmailValidation::class)->middleware('auth')->name('auth.email-validation');
 Route::get('/logout', fn () => auth()->logout());
 Route::get('/password/recovery', Password\Recovery::class)->name('password.recovery');
 Route::get('/password/reset', Password\Reset::class)->name('password.reset');
