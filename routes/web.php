845bbd2c98bda50b7629ddc54fbc3f673fd12587
matheus8695 <0821matheus@gmail.com>
2024-06-24 11:00:42 -0300

    CRM-31: create epic branch

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     #region Customers
     Route::get('/customers', Customers\Index::class)->name('customers');
+    Route::get('/customers/{customer}', fn () => 'oi')->name('customer.show');
     #endregion
 });
