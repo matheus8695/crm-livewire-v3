d559645b95b5ed63395fe3f27440362ab929281c
matheus8695 <0821matheus@gmail.com>
2024-07-15 15:55:42 -0300

    CRM-40: getting opportunities from database

--- app/Livewire/Opportunities/Board.php
+++ app/Livewire/Opportunities/Board.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire\Opportunities;
 
+use App\Models\Opportunity;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Board extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.opportunities.board');
+    }
+
+    #[Computed]
+    public function opportunities(): Collection
+    {
+        return Opportunity::query()->get();
     }
 }
--- resources/views/livewire/opportunities/board.blade.php
+++ resources/views/livewire/opportunities/board.blade.php
@@ -1,25 +1,12 @@
 <div class="p-2 grid grid-cols-3 gap-4 h-full">
-    <div class="bg-base-100 p-2 rounded-md">
-        <x-header title="Open" subtitle="Total 2 opportunities" size="text-lg" class="px-2 pb-0 mb-2" separator progress-indicator />
-        <div class="space-y-2 py-2 px-2">
-            <x-card>Opp-1</x-card>
-            <x-card>Opp-2</x-card>
+    @foreach ($this->opportunities->groupBy('status') as $status => $items)
+        <div class="bg-base-300 p-2 rounded-md">
+            <x-header :title="$status" subtitle="Total {{ $items->count() }} opportunities" size="text-lg" class="px-2 pb-0 mb-2" separator progress-indicator />
+            <div class="space-y-2 py-2 px-2">
+                @foreach ($items as $item)
+                    <x-card>{{ $item->title }}</x-card>
+                @endforeach
+            </div>
         </div>
-    </div>
-
-    <div class="bg-base-100 p-2 rounded-md">
-        <x-header title="Won" subtitle="Total 2 opportunities" size="text-lg" class="px-2 pb-0 mb-2" separator progress-indicator />
-        <div class="space-y-2 py-2 px-2">
-            <x-card>Opp-1</x-card>
-            <x-card>Opp-2</x-card>
-        </div>
-    </div>
-
-    <div class="bg-base-100 p-2 rounded-md">
-        <x-header title="Lost" subtitle="Total 2 opportunities" size="text-lg" class="px-2 pb-0 mb-2" separator progress-indicator />
-        <div class="space-y-2 py-2 px-2">
-            <x-card>Opp-1</x-card>
-            <x-card>Opp-2</x-card>
-        </div>
-    </div>
+    @endforeach
 </div>
