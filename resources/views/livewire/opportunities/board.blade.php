029af810f682833483970dec163e3461e6322884
matheus8695 <0821matheus@gmail.com>
2024-07-15 15:40:15 -0300

    CRM-40: board layout

---
+++ resources/views/livewire/opportunities/board.blade.php
@@ -0,0 +1,25 @@
+<div class="p-2 grid grid-cols-3 gap-4 h-full">
+    <div class="bg-base-100 p-2 rounded-md">
+        <x-header title="Open" subtitle="Total 2 opportunities" size="text-lg" class="px-2 pb-0 mb-2" separator progress-indicator />
+        <div class="space-y-2 py-2 px-2">
+            <x-card>Opp-1</x-card>
+            <x-card>Opp-2</x-card>
+        </div>
+    </div>
+
+    <div class="bg-base-100 p-2 rounded-md">
+        <x-header title="Won" subtitle="Total 2 opportunities" size="text-lg" class="px-2 pb-0 mb-2" separator progress-indicator />
+        <div class="space-y-2 py-2 px-2">
+            <x-card>Opp-1</x-card>
+            <x-card>Opp-2</x-card>
+        </div>
+    </div>
+
+    <div class="bg-base-100 p-2 rounded-md">
+        <x-header title="Lost" subtitle="Total 2 opportunities" size="text-lg" class="px-2 pb-0 mb-2" separator progress-indicator />
+        <div class="space-y-2 py-2 px-2">
+            <x-card>Opp-1</x-card>
+            <x-card>Opp-2</x-card>
+        </div>
+    </div>
+</div>
