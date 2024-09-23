d5ebf90492fce52aeae2f23379d60b1202fa2b87
matheus8695 <0821matheus@gmail.com>
2024-09-23 09:51:44 -0300

    CRM-44: query scope

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,9 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\{Builder, Model};
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $with = ['assignedTo'];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function assignedTo(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function scopeNotDone(Builder $query): Builder
+    {
+        return $query->whereNull('done_at');
+    }
+
+    public function scopeDone(Builder $query): Builder
+    {
+        return $query->whereNotNull('done_at');
+    }
 }
