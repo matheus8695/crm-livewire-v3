6bef9a4770993ad39c1bcd383f1b3e4bb04bdfd1
matheus8695 <0821matheus@gmail.com>
2024-11-01 11:10:44 -0300

    CRM-45: order tasks

--- app/Livewire/Customers/Tasks/Index.php
+++ app/Livewire/Customers/Tasks/Index.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Customers\Tasks;
 
 use App\Models\Customer;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 use Livewire\Attributes\{Computed, On};
 use Livewire\Component;
@@ -20,12 +21,18 @@ class Index extends Component
     #[Computed]
     public function notDoneTasks()
     {
-        return $this->customer->tasks()->notDone()->get();
+        return $this->customer->tasks()->orderBy('sort_order')->notDone()->get();
     }
 
     #[Computed]
     public function doneTasks()
     {
         return $this->customer->tasks()->done()->get();
+    }
+
+    public function updateTaskOrder($items)
+    {
+        $orders = collect($items)->pluck('value')->join(',');
+        DB::table('tasks')->update(['sort_order' => DB::raw("field(id, $orders)")]);
     }
 }
--- resources/views/livewire/customers/tasks/index.blade.php
+++ resources/views/livewire/customers/tasks/index.blade.php
@@ -2,15 +2,19 @@
     <livewire:customers.tasks.create :customer="$customer"/>
 
     <div class="uppercase font-bold text-slate-600 text-xs mb-2">Pending [{{ $this->notDoneTasks->count() }}]</div>
-    <ul class="flex flex-col gap-1 mb-6">
+    <ul class="flex flex-col gap-1 mb-6" wire:sortable='updateTaskOrder'>
         @foreach ($this->notDoneTasks as $task)
-        <li>
-            <input id="task-{{ $task->id }}" type="checkbox" value="1" @if ($task->done_at) checked @endif />
-            <label for="task-{{ $task->id }}">{{ $task->title }}</label>
-            <select>
-                <option>assigned to: {{ $task->assignedTo?->name }}</option>    
-            </select>   
-        </li>    
+            <li wire:sortable.item='{{ $task->id }}' wire:key='task-{{ $task->id }}'>
+                <button wire:sortable.handle title="{{ __('Drah and drop') }}">
+                    <x-icon name="o-queue-list" class="w-4 h-4 -mt-px opacity-30"/>
+                </button>
+
+                <input id="task-{{ $task->id }}" type="checkbox" value="1" @if ($task->done_at) checked @endif />
+                <label for="task-{{ $task->id }}">{{ $task->title }}</label>
+                <select>
+                    <option>assigned to: {{ $task->assignedTo?->name }}</option>    
+                </select>   
+            </li>    
         @endforeach
     </ul>
     
